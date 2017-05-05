9b6111ec1a700a31da3ab236b30212ab54b064a1
mishiki1002 <Brownbear1002@gmail.com>
2017-05-04 21:36:13 -0400

    Completed choose task and setup of for the auto car vehicle

--- src/chassis.cpp
+++ src/chassis.cpp
@@ -2,18 +2,50 @@
 
 Chassis::Chassis(){
 
-  MotorController = 5;
-  SteerController = 9;
+  this->MotorControllerPin = 5;
+  this->SteerControllerPin = 9;
 
-  LeftEncoder = 18;
-  RightEncoder = 19;
+  this->LeftEncoderPin = 18;
+  this->RightEncoderPin = 19;
 }
 
-Chassis::initalize(){
-  Encoder Motor_Encoder(LeftEncoder, RightEncoder);
-  Servo Motor_Controller(MotorController);
-}
 
 Chassis::~Chassis(){
-  
+
+}
+
+int Chassis::chooseTask(){
+  int sum = 0;
+  int val;
+  int binaryVal;
+
+  if(int i = 0; i < numSwitches; i++){
+    //Get the binary number
+    binaryVal *= 2;
+    if(binaryVal == 0)
+    {
+      binaryVal = 1;
+    }
+  }
+  return sum;
+}
+
+void Chassis::initalize(){
+  Motor_Controller.atach(this->MotorControllerPin);
+  Steer_Controller.attach(this->SteerControllerPin);
+  Motor_Encoder(this->LeftEncoderPin, this->RightEncoderPin);
+
+  //Initalizing Binary switchs
+  if(int 1 = 0; i < Max_Switchs; i++){
+    //Send Switch a initalization Volt of high
+    pinMode(taskSwitches[i], OUTPUT);
+    //Then send it to low to tell it that it is off
+    digitalWrite(taskSwitches[i], LOW);
+    /*
+    Then we tell the switch to start with the internal pull up resistors
+    When a switch is swiched on there is no voltage, When its off there is
+    No volts
+    */
+    pinMode(taskSwitches[i], INPUT_PULLUP);
+  }
 }
--- src/chassis.h
+++ src/chassis.h
@@ -1,18 +1,27 @@
 #ifndef CHASSIS_H
 #define CHASSIS_H
+
+#include <Servo.h>
+#include <Encoder.h>
+
 class Chassis{
 public:
   Chassis();
   virtual ~Chassis();
 
   void initalize;
+  int choseTask;
 private:
   //Motor Controller for both Left and Right
-  int MotorController;
+  int MotorControllerPin;
   //Steer Controller, The Driving System
-  int SteerController;
+  int SteerControllerPin;
 
-  int LeftEncoder;
-  int RightEncoder;
+  int LeftEncoderPin;
+  int RightEncoderPin;
+
+  Encoder Motor_Encoder;
+  Servo Motor_Controller;
+  Servo Steer_Controller;
 }
 #endif
