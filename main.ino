130c80928d85b0dba23d9a076fd5e51322d304a1
mishiki1002 <Brownbear1002@gmail.com>
2017-06-13 13:32:24 -0400

    Added Skeleton in c++ but untested

--- main.ino
+++ main.ino
@@ -1,9 +1,42 @@
 #include "src/chassis.h"
 
-void setup(){
+Chassis jeep;
 
+int task = 0;
+
+void setup(){
+  jeep = new Chassis();
+
+  jeep.initalize();
 }
 
 void loop(){
-  
+  switch (task) {
+    case 1:
+      
+      break;
+    case 2:
+
+      break;
+
+    case 3:
+
+      break;
+
+    case 4:
+
+      break;
+
+    case 5:
+
+      break;
+
+    case 6:
+
+      break;
+
+    case 7:
+
+      break;
+  }
 }
