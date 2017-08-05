da0518231a5870c82a0caaddcd02f48a1f44f0d2
mishiki1002 <brownbear1002@gamil.com>
2017-08-05 14:56:17 -0400

    Cleaned Folders

--- main.ino
+++ main.ino
@@ -1,42 +1,90 @@
-#include "src/chassis.h"
+#include <Encoder.h>
+#include "NAxisMotion.h"
+#include <Wire.h>
+#include <Servo.h>
+#include <SPI.h>
+#include <Pixy.h>
 
-Chassis jeep;
+//Objects
+NAxisMotion motionShield;
+Pixy camera;
+Servo motor;
+Servo steer;
+Servo Sweeper;
 
-int task = 0;
+//Motors
+//Pin numbers for ease of access
+int encLeftPin = 18;
+int encRightPin = 19;
+int drivePin = 5;
+int steerPin = 9;
+int frSweeperPin = 12;
+int frSensorPin = 3;
 
-void setup(){
-  jeep = new Chassis();
+//Paramaters
+//Some commonly used steering values
+int rightTurnStart = 96;
+int rightTurnEnd = 60;
+int leftTurnStart = 72;
+int leftTurnEnd = 67;
 
-  jeep.initalize();
+//Values for the steering motor
+//Remember that it is sideways so you
+int maxSteer = 150;
+int midSteer = 90;
+int minSteer = 35;
+
+//Debuging Data
+bool firstRun = true;
+bool debugMode = true;
+
+//What task to run
+int task;
+
+//Binary Code to run for selectin which task to run
+//0 = nothing
+int taskSwitches[] = {
+    6,
+    7,
+    8
 }
 
-void loop(){
-  switch (task) {
-    case 1:
+//Bling
+int headLights = 16;
+int tailLights = 15;
 
-      break;
-    case 2:
+//Max Switches on board
+int switches = 3;
 
-      break;
+Matbotix frSensor(frSensorPin, Maxbotix::PW, Maxbotix::LV, Maxbotix::BEST);
+Encoder enc(encLeftPin, encRightPin);
 
-    case 3:
 
-      break;
+int feetToInches(int feet){
+    return feet * 12;
+}
 
-    case 4:
+void setup(){
+    pinMode(headLights, OUTPUT);
+    pinMode(tailLights, OUTPUT);
+    digitalWrite(headLights, HIGH);
+    digitalWrite(tailLights, HIGH);
 
-      break;
+    motor.attach(drivePin);
+    steer.attach(steerPin, 700, 2300);
+    frSweep.attach(frSweepPin);
 
-    case 5:
+    //Start serial communications at 9000 buad rate
+    Serial.begin(9000);
+    Serial.println("Serial Comm Begin");
 
-      break;
+    pixy.init();
+    I2C.begin();
+    motionShield.initSensor();
+    motionShield.setOperationMode(OPERATION_MODE_NDOF);
+    motionShield.setUpdateMode(MANUAL);
 
-    case 6:
-
-      break;
-
-    case 7:
-
-      break;
-  }
+    for(int i = 0; i < numSwitches; i++){
+        
+    }
 }
